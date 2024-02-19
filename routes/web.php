ab83a8aa03469f57677b3145945126169af0f997
011223345aA <huyhoang11202@gmail.com>
2024-02-19 23:26:32 +0700

    add template admin

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-=======
 Route::get('/index', function () {
     return view('home.user');
 });
 
->>>>>>> 8541258 (add template home page)
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
