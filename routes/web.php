6c43a35ce52dc57b2d5ff6c531f351da6891c21f
011223345aA <huyhoang11202@gmail.com>
2024-02-22 21:32:27 +0700

    add Middleware to Auth

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,17 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('/view_category', [\App\Http\Controllers\AdminController::class, 'view_category']);
+    Route::post('/add_category', [\App\Http\Controllers\AdminController::class, 'add_category']);
+    Route::get('/delete_category/{id}', [\App\Http\Controllers\AdminController::class, 'delete_category']);
 
-Route::get('/view_category',[\App\Http\Controllers\AdminController::class,'view_category']);
-Route::post('/add_category',[\App\Http\Controllers\AdminController::class,'add_category']);
-Route::get('/delete_category/{id}',[\App\Http\Controllers\AdminController::class,'delete_category']);
+    Route::get('/view_product',[\App\Http\Controllers\ProductController::class,'view_product']);
+    Route::post('/add_product',[\App\Http\Controllers\ProductController::class,'add_product']);
+    Route::get('/show_product',[\App\Http\Controllers\ProductController::class,'show_product']);
+    Route::get('/delete_product/{id}',[\App\Http\Controllers\ProductController::class,'delete_product']);
+    Route::get('/edit_product/{id}',[\App\Http\Controllers\ProductController::class,'edit_product']);
+    Route::post('/updateProductConfirm/{id}',[\App\Http\Controllers\ProductController::class,'updateProductConfirm']);
+});
 
-Route::get('/view_product',[\App\Http\Controllers\ProductController::class,'view_product']);
-Route::post('/add_product',[\App\Http\Controllers\ProductController::class,'add_product']);
-Route::get('/show_product',[\App\Http\Controllers\ProductController::class,'show_product']);
-Route::get('/delete_product/{id}',[\App\Http\Controllers\ProductController::class,'delete_product']);
-Route::get('/edit_product/{id}',[\App\Http\Controllers\ProductController::class,'edit_product']);
-Route::post('/updateProductConfirm/{id}',[\App\Http\Controllers\ProductController::class,'updateProductConfirm']);
+
